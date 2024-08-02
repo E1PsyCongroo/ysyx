0ec51ceb6f774e13f890efcb76018dcc7e390f8a
tracer-ysyx <tracer@ysyx.org>
2024-08-02 15:51:41 +0800

    > generate verilog ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 27 Jul 2024 16:49:32 +0000 x86_64 GNU/Linux  15:51:41 up  5:33,  2 users,  load average: 2.43, 2.92, 2.89

--- npc/src/main/scala/RV32E/Control.scala
+++ npc/src/main/scala/RV32E/Control.scala
@@ -324,11 +324,10 @@ class Control extends Module {
   import Instruction.InstricitonMap._
 
   val possiblePatterns = Seq(
-    JAL
-    // LUI, AUIPC, JAL, JALR, BEQ, BNE, BLT, BGE, BLTU, BGEU,
-    // LB, LH, LW, LBU, LHU, SB, SH, SW, ADDI, SLTI, SLTIU,
-    // XORI, ORI, ANDI, SLLI, SRLI, SRAI, ADD, SUB, SLL, SLT,
-    // SLTU, XOR, SRL, SRA, OR, AND, FENCE, ECALL, EBREAK
+    LUI, AUIPC, JAL, JALR, BEQ, BNE, BLT, BGE, BLTU, BGEU,
+    LB, LH, LW, LBU, LHU, SB, SH, SW, ADDI, SLTI, SLTIU,
+    XORI, ORI, ANDI, SLLI, SRLI, SRAI, ADD, SUB, SLL, SLT,
+    SLTU, XOR, SRL, SRA, OR, AND, FENCE, ECALL, EBREAK
   )
   val decodeTable = new DecodeTable(
     possiblePatterns,
