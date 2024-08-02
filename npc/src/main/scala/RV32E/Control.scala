941abad6828b01f7bc1b70581f2bd1d379ed2510
tracer-ysyx <tracer@ysyx.org>
2024-08-02 15:40:37 +0800

    > generate verilog ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 27 Jul 2024 16:49:32 +0000 x86_64 GNU/Linux  15:40:37 up  5:22,  2 users,  load average: 1.64, 2.77, 2.59

--- npc/src/main/scala/RV32E/Control.scala
+++ npc/src/main/scala/RV32E/Control.scala
@@ -73,7 +73,6 @@ object Instruction {
 
   val instrTypeMap = Map(
     BitPat("b0110111") -> InstructionType.UType,
-    BitPat("b0110111") -> InstructionType.UType,
     BitPat("b0010111") -> InstructionType.UType,
     BitPat("b1101111") -> InstructionType.JType,
     BitPat("b1100111") -> InstructionType.IType,
@@ -336,24 +335,34 @@ class Control extends Module {
     possiblePatterns,
     Seq(
       ImmControlField,
-      RegWeControlField,
-      ALUControlField,
-      BrControlField,
-      MemOpControlField,
-      MemWenControlField,
-      ALUASrcControlField,
-      ALUBSrcControlField,
-      WBSrcControlField
+      // RegWeControlField,
+      // ALUControlField,
+      // BrControlField,
+      // MemOpControlField,
+      // MemWenControlField,
+      // ALUASrcControlField,
+      // ALUBSrcControlField,
+      // WBSrcControlField
     )
   )
   val decodeResult = decodeTable.decode(io.instr)
-  io.immType      := decodeResult(ImmControlField)
-  io.regWe        := decodeResult(RegWeControlField)
-  io.aluASrc      := decodeResult(ALUASrcControlField)
-  io.aluBSrc      := decodeResult(ALUBSrcControlField)
-  io.aluCtr       := decodeResult(ALUControlField)
-  io.brType       := decodeResult(BrControlField)
-  io.wbSrc        := decodeResult(WBSrcControlField)
-  io.memWe        := decodeResult(MemWenControlField)
-  io.memOp        := decodeResult(MemOpControlField)
+  // io.immType      := decodeResult(ImmControlField)
+  // io.regWe        := decodeResult(RegWeControlField)
+  // io.aluASrc      := decodeResult(ALUASrcControlField)
+  // io.aluBSrc      := decodeResult(ALUBSrcControlField)
+  // io.aluCtr       := decodeResult(ALUControlField)
+  // io.brType       := decodeResult(BrControlField)
+  // io.wbSrc        := decodeResult(WBSrcControlField)
+  // io.memWe        := decodeResult(MemWenControlField)
+  // io.memOp        := decodeResult(MemOpControlField)
+
+  io.immType      := DontCare
+  io.regWe        := DontCare
+  io.aluASrc      := DontCare
+  io.aluBSrc      := DontCare
+  io.aluCtr       := DontCare
+  io.brType       := DontCare
+  io.wbSrc        := DontCare
+  io.memWe        := DontCare
+  io.memOp        := DontCare
 }
