e85ad17dee4ca1f0e6475f071d2495353a4479e0
tracer-ysyx <tracer@ysyx.org>
2024-08-02 19:38:01 +0800

    > sim RTL ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 27 Jul 2024 16:49:32 +0000 x86_64 GNU/Linux  19:38:01 up  9:19,  2 users,  load average: 2.25, 2.23, 1.91

--- npc/src/main/csrc/common.h
+++ npc/src/main/csrc/common.h
@@ -16,9 +16,9 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#include <stdint.h>
-#include <inttypes.h>
-#include <stdbool.h>
+#include <cstdint>
+#include <cinttypes>
+#include <cstdbool>
 #include <string.h>
 
 #include <macro.h>
