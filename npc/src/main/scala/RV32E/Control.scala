6c8196cd260fa5b5bbd4700ea355b09b8ef203a0
tracer-ysyx <tracer@ysyx.org>
2024-08-02 15:15:59 +0800

    > generate verilog ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 27 Jul 2024 16:49:32 +0000 x86_64 GNU/Linux  15:15:59 up  4:57,  2 users,  load average: 2.57, 2.52, 2.02

--- npc/src/main/scala/RV32E/Control.scala
+++ npc/src/main/scala/RV32E/Control.scala
@@ -310,7 +310,7 @@ class Control extends Module {
   val possiblePatterns = Seq(
     // LUI, AUIPC, JAL, JALR, BEQ, BNE, BLT, BGE, BLTU, BGEU,
     LB, LH, LW, LBU, LHU, SB, SH, SW, ADDI, SLTI, SLTUI,
-    XORI, ORI, ANDI, SLLI, SRLI, SRAI, ADD, SUB, SLL, SLT,
+    // XORI, ORI, ANDI, SLLI, SRLI, SRAI, ADD, SUB, SLL, SLT,
     // SLTU, XOR, SRL, SRA, OR, AND, FENCE, ECALL, EBREAK
   )
   val decodeTable = new DecodeTable(
