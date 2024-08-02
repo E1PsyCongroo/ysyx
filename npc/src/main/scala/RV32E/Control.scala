a6810acd3eee5e213f54cbf1973a1a2f8cf01c43
tracer-ysyx <tracer@ysyx.org>
2024-08-02 15:49:28 +0800

    > generate verilog ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.2-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Sat, 27 Jul 2024 16:49:32 +0000 x86_64 GNU/Linux  15:49:28 up  5:31,  2 users,  load average: 2.51, 3.10, 2.93

--- npc/src/main/scala/RV32E/Control.scala
+++ npc/src/main/scala/RV32E/Control.scala
@@ -99,7 +99,7 @@ object ImmControlField extends DecodeField[ControlPattern, UInt] {
   def name = "Imm Control Field"
   def chiselType: UInt = UInt(ImmType.getWidth.W)
   def genTable(op: ControlPattern): BitPat = {
-    val dontCare = BitPat.dontCare(ImmType.getWidth.W)
+    val dontCare = BitPat.dontCare(ImmType.getWidth)
     Instruction.instrTypeMap.get(op.opcode) match {
       case Some(instrType) => Instruction.immTypeMap.get(instrType) match {
         case Some(immType) => BitPat(immType.litValue.U(ImmType.getWidth.W))
