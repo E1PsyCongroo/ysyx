489194f32b936b430a14b483c45b028724977cf8
focused_xy <elpsycongroo242@gmail.com>
2024-08-07 16:20:42 +0800

    PA2-stage2 fin

---
+++ npc/scripts/build.mk
@@ -0,0 +1,103 @@
+.DEFAULT_GOAL = app
+
+# Add necessary options if the target is a shared library
+ifeq ($(SHARE),1)
+SO = -so
+CFLAGS  += -fPIC -fvisibility=hidden
+LDFLAGS += -shared -fPIC
+endif
+
+PRJ 														:= RVCPU
+WORK_DIR  											:= $(shell pwd)
+BUILD_DIR 											:= $(WORK_DIR)/build
+OBJ_DIR  												:= $(BUILD_DIR)/obj-$(NAME)$(SO)
+SRC_DIR													:= $(WORK_DIR)/src/main
+VSRC_DIR 												:= $(SRC_DIR)/vsrc
+CSRC_DIR 												:= $(SRC_DIR)/csrc
+RESOURCES_DIR										:= $(SRC_DIR)/resources
+CONSTR_DIR											:= $(SRC_DIR)/constr
+CHISEL_SRC_DIR									:= $(SRC_DIR)/scala
+
+INC_PATH 												:= $(WORK_DIR)/include $(INC_PATH)
+BINARY   												:= $(BUILD_DIR)/$(NAME)$(SO)
+
+
+# project source
+VSRCS 													?= $(shell find $(VSRC_DIR) -type f -name "*.v" -or -name "*.sv")
+CHISELSRCS											?= $(shell find $(CHISEL_SRC_DIR) -type f -name "*.scala" -or -name "*.sc")
+RESOURCES												?= $(shell find $(RESOURCES_DIR) -type f -name "*.v" -or -name "*.sv")
+
+# rules for NVBoard
+include $(NVBOARD_HOME)/scripts/nvboard.mk
+
+# Compilation flags
+ifeq ($(CC),clang)
+CXX 														:= clang++
+else
+CXX 														:= g++
+endif
+LD 															:= $(CXX)
+INCLUDES 												= $(addprefix -I, $(INC_PATH))
+CFLAGS  												:= -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
+LDFLAGS 												:= -O2 $(LDFLAGS)
+
+# Verilator flags
+VERILATOR 											:= verilator
+VERILATOR_CFLAGS 								?= --MMD --build --cc -O3 --x-assign fast --x-initial fast --noassert --trace
+
+# Verilating
+$(BUILD_DIR)/.stamp.verilog: $(CHISELSRCS)
+	$(call git_commit, "generate verilog")
+	@echo + VERILOG $(VSRC_DIR)
+	@mill -i $(PRJ).runMain Elaborate --target-dir $(VSRC_DIR)
+	@touch $@
+
+$(BUILD_DIR)/lib$(PRJ).a: $(BUILD_DIR)/.stamp.verilator $(RESOURCES)
+	@echo + VERILATOR $(RESOURCES) $(VSRCS)
+	@mkdir -p $(OBJ_DIR)
+	@$(VERILATOR) $(VERILATOR_CFLAGS) \
+		--top-module $(PRJ) $(RESOURCES) $(VSRCS) \
+		--lib-create $(PRJ) --Mdir $(OBJ_DIR)
+
+# NVBOARD
+$(BUILD_DIR)/$(PRJ)_auto_bind.cc: $(CONSTR_DIR)/$(PRJ).nxdc
+	@echo + AUTO-BIND $<
+	@mkdir -p $(dir $@)
+	@python3 $(NVBOARD_HOME)/scripts/auto_pin_bind.py $^ $@
+
+CXXSRC += $(BUILD_DIR)/$(PRJ)_auto_bind.cc
+ARCHIVES += $(BUILD_DIR)/lib$(PRJ).a $(NVBOARD_ARCHIVE)
+OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
+
+# Compilation patterns
+$(OBJ_DIR)/%.o: %.c
+	@echo + CC $<
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) -c -o $@ $<
+	$(call call_fixdep, $(@:.o=.d), $@)
+
+$(OBJ_DIR)/%.o: %.cc
+	@echo + CXX $<
+	@mkdir -p $(dir $@)
+	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
+	$(call call_fixdep, $(@:.o=.d), $@)
+
+# Depencies
+-include $(OBJS:.o=.d)
+
+# Some convenient rules
+
+.PHONY: app verilog verilator clean
+
+app: $(BINARY)
+
+$(BINARY):: $(OBJS) $(ARCHIVES)
+	@echo + LD $@
+	@$(LD) -o $@ $(OBJS) $(LDFLAGS) $(ARCHIVES) $(LIBS)
+
+verilog: $(BUILD_DIR)/.stamp.verilog
+
+verilator: $(BUILD_DIR)/lib$(PRJ).a
+
+clean:
+	-rm -rf $(BUILD_DIR)
---
+++ npc/scripts/native.mk
@@ -0,0 +1,56 @@
+-include $(NPC_HOME)/../Makefile
+include $(NPC_HOME)/scripts/build.mk
+
+include $(NPC_HOME)/tools/difftest.mk
+
+compile_git:
+	$(call git_commit, "compile NPC")
+$(BINARY):: compile_git
+
+# Some convenient rules
+
+override ARGS ?= --log=$(BUILD_DIR)/npc-log.txt
+ifdef ELF
+override ARGS += --elf=$(ELF)
+endif
+override ARGS += $(ARGS_DIFF)
+
+# Command to execute NPC
+IMG ?=
+NPC_EXEC := $(BINARY) $(ARGS) $(IMG)
+
+test:
+	mill -i $(PRJ).test
+
+mill-help:
+	mill -i $(PRJ).runMain Elaborate --help
+
+reformat:
+	mill -i __.reformat
+
+checkformat:
+	mill -i __.checkFormat
+
+bsp:
+	mill -i mill.bsp.BSP/install
+
+idea:
+	mill -i mill.idea.GenIdea/idea
+
+run-env: $(BINARY) $(DIFF_REF_SO)
+
+run: run-env
+	$(call git_commit, "run NPC")
+	$(NPC_EXEC)
+
+gdb: run-env
+	$(call git_commit, "gdb NPC")
+	gdb -s $(BINARY) --args $(NPC_EXEC)
+
+clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
+$(clean-tools):
+	-@$(MAKE) -s -C $@ clean
+clean-tools: $(clean-tools)
+clean-all: clean distclean clean-tools
+
+.PHONY: run gdb run-env clean-tools clean-all $(clean-tools) test help reformat checkformat
