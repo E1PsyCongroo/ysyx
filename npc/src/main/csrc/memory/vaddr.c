c0511473989ab8085abf1d963989411929a2264e
tracer-ysyx <tracer@ysyx.org>
2024-08-09 11:38:54 +0800

    > compile NPC ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.3-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 06 Aug 2024 07:47:21 +0000 x86_64 GNU/Linux  11:38:54 up  1:51,  2 users,  load average: 2.18, 2.25, 2.07

--- npc/src/main/csrc/memory/vaddr.c
+++ npc/src/main/csrc/memory/vaddr.c
@@ -3,9 +3,13 @@
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
 #ifdef CONFIG_MTRACE
-  if (MTRACE_COND) { log_write(ANSI_FMT("Read instruction:\t@" FMT_PADDR ", len = %2d\n", ANSI_FG_CYAN), addr, len); }
+  if (MTRACE_COND) { log_write(ANSI_FMT("Read instruction:\t@" FMT_PADDR ", len = %2d, ", ANSI_FG_CYAN), addr, len); }
 #endif
-  return paddr_read(addr, len);
+  word_t inst = paddr_read(addr, len);
+#ifdef CONFIG_MTRACE
+  if (MTRACE_COND) { log_write(ANSI_FMT("inst = " FMT_WORD "\n", ANSI_FG_CYAN), inst); }
+#endif
+  return inst;
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
