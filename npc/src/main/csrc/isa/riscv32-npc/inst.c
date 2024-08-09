902b46547356809f1678bf379078b6c9d464fcc3
tracer-ysyx <tracer@ysyx.org>
2024-08-09 10:59:10 +0800

    > compile NPC ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.3-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 06 Aug 2024 07:47:21 +0000 x86_64 GNU/Linux  10:59:10 up  1:11,  2 users,  load average: 1.80, 1.54, 1.99

--- npc/src/main/csrc/isa/riscv32-npc/inst.c
+++ npc/src/main/csrc/isa/riscv32-npc/inst.c
@@ -2,9 +2,11 @@
 #include "local-include/verilating.h"
 #include <cpu/cpu.h>
 #include <cpu/decode.h>
+#include <cpu/ifetch.h>
 
 
 int isa_exec_once(Decode *s) {
+  s->isa.inst.val = vaddr_ifetch(*(cpu.pc), 4);
   rvcpu_single_cycle();
   s->dnpc = *(cpu.pc);
   return 0;
