7e93c3a1f18a5131501b52f7c980f0b9ac9e1cf8
tracer-ysyx <tracer@ysyx.org>
2024-08-09 16:03:23 +0800

    > compile NPC ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.3-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 06 Aug 2024 07:47:21 +0000 x86_64 GNU/Linux  16:03:23 up  6:16,  2 users,  load average: 4.62, 4.97, 5.12

--- npc/src/main/csrc/isa/riscv32-npc/verilating.cc
+++ npc/src/main/csrc/isa/riscv32-npc/verilating.cc
@@ -99,6 +99,7 @@ void rvcpu_single_cycle(void) {
   rvcpu->clock = 1; rvcpu->eval();
   rvcpu->io_inst = vaddr_ifetch(pc, 4); rvcpu->eval();
   contextp->timeInc(1); tfp->dump(contextp->time());
+  rvcpu_sync();
   // printf("pc: " FMT_WORD ", inst: " FMT_WORD "\n", rvcpu->rootp->RVCPU__DOT__PC, rvcpu->io_inst);
   /* time down */
   rvcpu->clock = 0; rvcpu->eval();
