07f730e30d93c8f4042964b314aa2c067eac4ad2
tracer-ysyx <tracer@ysyx.org>
2024-08-09 12:20:42 +0800

    > compile NEMU ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.3-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 06 Aug 2024 07:47:21 +0000 x86_64 GNU/Linux  12:20:42 up  2:33,  2 users,  load average: 2.28, 2.00, 1.98

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -17,13 +17,26 @@
 #include <memory/paddr.h>
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  word_t inst = paddr_read(addr, len);
+#ifdef CONFIG_MTRACE
+  if (MTRACE_COND) { log_write(ANSI_FMT("%-16.16s: @" FMT_PADDR ", len = %2d, inst = " FMT_WORD "\n", ANSI_FG_CYAN), "Read Instruction", addr, len, inst); }
+#endif
+  return inst;
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  word_t data = paddr_read(addr, len);
+#ifdef CONFIG_MTRACE
+  if (MTRACE_COND) { log_write(ANSI_FMT("%-16.16s: @" FMT_PADDR ", len = %2d, data = " FMT_WORD "\n", ANSI_FG_CYAN), "Read Memory", addr, len, data); }
+#endif
+  return data;
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  if (MTRACE_COND) { log_write(ANSI_FMT("%-16.16s: @" FMT_PADDR ", len = %2d, data = " FMT_WORD "\n", ANSI_FG_CYAN), "Write Memory", addr, len, data); }
+#endif
   paddr_write(addr, len, data);
 }
+
+
