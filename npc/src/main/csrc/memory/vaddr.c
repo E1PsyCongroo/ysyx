bcbac4f3f0a99c788c8679403cbb1944df580d81
tracer-ysyx <tracer@ysyx.org>
2024-08-09 11:37:28 +0800

    > compile NPC ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.3-zen1-2-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 06 Aug 2024 07:47:21 +0000 x86_64 GNU/Linux  11:37:28 up  1:50,  2 users,  load average: 2.90, 2.32, 2.07

--- npc/src/main/csrc/memory/vaddr.c
+++ npc/src/main/csrc/memory/vaddr.c
@@ -3,14 +3,14 @@
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
 #ifdef CONFIG_MTRACE
-  if (MTRACE_COND) { log_write(ANSI_FMT("Read instruction: @" FMT_PADDR ", len = %2d\n", ANSI_FG_CYAN), addr, len); }
+  if (MTRACE_COND) { log_write(ANSI_FMT("Read instruction:\t@" FMT_PADDR ", len = %2d\n", ANSI_FG_CYAN), addr, len); }
 #endif
   return paddr_read(addr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
 #ifdef CONFIG_MTRACE
-  if (MTRACE_COND) { log_write(ANSI_FMT("Read memory: @" FMT_PADDR ", len = %2d, ", ANSI_FG_CYAN), addr, len ); }
+  if (MTRACE_COND) { log_write(ANSI_FMT("Read memory:\t@" FMT_PADDR ", len = %2d, ", ANSI_FG_CYAN), addr, len ); }
 #endif
   word_t data = paddr_read(addr, len);
 #ifdef CONFIG_MTRACE
@@ -21,7 +21,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
 #ifdef CONFIG_MTRACE
-  if (MTRACE_COND) { log_write(ANSI_FMT("Write memory: @" FMT_PADDR ", len = %2d, data = " FMT_WORD "\n", ANSI_FG_CYAN), addr, len, data); }
+  if (MTRACE_COND) { log_write(ANSI_FMT("Write memory:\t@" FMT_PADDR ", len = %2d, data = " FMT_WORD "\n", ANSI_FG_CYAN), addr, len, data); }
 #endif
   paddr_write(addr, len, data);
 }
