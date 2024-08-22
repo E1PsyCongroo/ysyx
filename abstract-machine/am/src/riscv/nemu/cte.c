01182517f06a4513dfc551edca508c37d0c91104
tracer-ysyx <tracer@ysyx.org>
2024-08-22 11:19:33 +0800

    > compile NEMU ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.6-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:05 +0000 x86_64 GNU/Linux  11:19:33 up  1:45,  2 users,  load average: 2.81, 2.46, 2.53

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,7 +5,6 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  printf("%#x %#x %#x\n", c->mepc, c->mstatus, c->mcause);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
