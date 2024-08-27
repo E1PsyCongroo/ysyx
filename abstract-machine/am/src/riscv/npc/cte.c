d976daab4986b006a04b3c071d5dce14ec7ae17b
tracer-ysyx <tracer@ysyx.org>
2024-08-27 12:46:48 +0800

    > compile NPC ysyx_22040000 Focused晓阳 Linux FocusedXYArchlinuxLaptop 6.10.6-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:05 +0000 x86_64 GNU/Linux  12:46:48 up  2:34,  2 users,  load average: 2.02, 1.93, 1.74

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -8,13 +8,14 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 8: case 9: case 11: ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-
+  c->mepc += 4;
   return c;
 }
 
@@ -31,7 +32,11 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  Context* ctx = (Context*)kstack.end - 1;
+  ctx->mstatus = 0x1800;
+  ctx->mepc = (uintptr_t)entry - 4;
+  ctx->gpr[10] = (uintptr_t)arg;
+  return ctx;
 }
 
 void yield() {
