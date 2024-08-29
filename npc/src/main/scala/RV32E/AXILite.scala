eea4c97f0a189b20faea8a612a2048b1af5721ba
focused_xy <elpsycongroo242@gmail.com>
2024-08-29 20:03:12 +0800

    feat(npc): add AXILiteMem

--- npc/src/main/scala/RV32E/AXILite.scala
+++ npc/src/main/scala/RV32E/AXILite.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 import chisel3.util.experimental.decode._
 
-class AXILiteIO(xlen: Int = 32) extends Bundle {
+class AXILiteIO(awith:Int = 32, dwith: Int = 32) extends Bundle {
   /* Global */
   val aclk    = Input(Clock())
   val aresetn = Input(Reset())
@@ -13,14 +13,14 @@ class AXILiteIO(xlen: Int = 32) extends Bundle {
   val awvalid = Input(Bool())
   val awready = Output(Bool())
   val awid    = Input(UInt(0.W))
-  val awaddr  = Input(UInt(xlen.W))
+  val awaddr  = Input(UInt(awith.W))
   val awport  = Input(UInt(3.W))
 
   /* Write data channel */
   val wvalid  = Input(Bool())
   val wready  = Output(Bool())
-  val wdata   = Input(UInt(xlen.W))
-  val wstarb  = Input(UInt((xlen/8).W))
+  val wdata   = Input(UInt(dwith.W))
+  val wstarb  = Input(UInt((dwith/8).W))
 
   /* Write response channel */
   val bvalid  = Output(Bool())
@@ -32,13 +32,20 @@ class AXILiteIO(xlen: Int = 32) extends Bundle {
   val arvalid = Input(Bool())
   val arready = Output(Bool())
   val arid    = Input(UInt(0.W))
-  val araddr  = Input(UInt(xlen.W))
+  val araddr  = Input(UInt(awith.W))
   val arport  = Input(UInt(3.W))
 
   /* Read data channel */
   val rvalid  = Output(Bool())
   val rready  = Input(Bool())
   val rid     = Output(UInt(0.W))
-  val rdata   = Output(UInt(xlen.W))
+  val rdata   = Output(UInt(dwith.W))
   val rresp   = Output(UInt(2.W))
+}
+
+object TransactionResponse extends ChiselEnum{
+  val okey    = Value("b00".U)
+  val exokay  = Value("b01".U)
+  val slverr  = Value("b10".U)
+  val decerr  = Value("b11".U)
 }
--- npc/src/main/scala/RV32E/Mem.scala
+++ npc/src/main/scala/RV32E/Mem.scala
@@ -4,7 +4,6 @@ import chisel3._
 import chisel3.util._
 import chisel3.util.experimental.decode._
 
-
 class MemIO extends Bundle {
   val ren     = Input(Bool())
   val raddr   = Input(UInt(32.W))
@@ -23,6 +22,102 @@ class Mem extends BlackBox with HasBlackBoxResource {
   addResource("/Mem.sv")
 }
 
-class AXILiteMem extends Module {
+class AXILiteMem(awith:Int = 32, dwith:Int = 32, size:Int = 4) extends RawModule {
+  require(log2Ceil(size) < awith)
+  require(log2Ceil(size) < dwith)
+
+  val io = IO(new AXILiteIO(awith, dwith))
+  val Mem = Module(new Mem)
+withClockAndReset (io.aclk, !io.aresetn.asBool) {
+  io.awid := DontCare // for manager is optional, so not realised
+  io.bid  := DontCare // for manager is optional, so not realised
+  io.rid  := DontCare // for manager is optional, so not realised
+
+  io.awport := DontCare // for subordinate is optional, so not realised
+  io.arport := DontCare // for subordinate is optional, so not realised
+
+  val awfire = io.awvalid && io.awready
+  val wfire  = io.wvalid && io.wready
+  val bfire  = io.bvalid && io.bready
+  val arfire = io.arvalid && io.arready
+  val rfire  = io.rvalid && io.rready
+
+  val sIdle :: sWaitWaddr :: sWaitWdata :: sWrite :: sRead :: Nil = Enum(5)
+  val state = RegInit(sIdle)
+  state := MuxLookup(state, sIdle)(Seq(
+    sIdle       -> MuxCase(sIdle, Seq(
+      arfire -> sRead,
+
+      (awfire && wfire) -> sWrite,
+      wfire  -> sWaitWaddr,
+      awfire -> sWaitWdata,
+    )),
+    sWaitWaddr  -> Mux(awfire, sWrite, sWaitWaddr),
+    sWaitWdata  -> Mux(wfire, sWrite, sWaitWdata),
+    sWrite      -> Mux(bfire, sIdle, sWrite),
+    sRead       -> Mux(rfire, sIdle, sRead),
+  ))
+
+  val isIdle  = state === sIdle
+  val isWaitWaddr = state === sWaitWaddr
+  val isWaitWdata = state === sWaitWdata
+  val isWrite = state === sWrite
+  val isRead  = state === sRead
+
+  /* Write address channel */
+  val awready     = RegInit(true.B)
+  awready         := isIdle || isWaitWaddr
+
+  val writeAddr   = RegEnable(io.awaddr, awfire)
+  val alignedWriteAddr = writeAddr(awith-1, log2Ceil(size)) ## Fill(log2Ceil(size), "b0".U)
+  val writeAddrAligned = writeAddr === alignedWriteAddr
+  assert(writeAddrAligned)
+
+  /* Write data channel */
+  val wready      = RegInit(true.B)
+  wready          := isIdle || isWaitWdata
+
+  val writeData   = RegEnable(io.wdata, wfire)
+  val writeMask   = RegEnable(io.wstarb, wfire)
+
+  /* Write response channel */
+  val bvalid      = RegEnable(Bool(), false.B, isWrite)
+  val bresp       = RegEnable(UInt(2.W), "b00".U(2.W), isRead)
+  bvalid          := Mux(bfire, false.B, true.B)
+  bresp           := TransactionResponse.okey
+  Mem.io.waddr    := Mux(awfire, io.awaddr, writeAddr)
+  Mem.io.wdata    := Mux(wfire, io.wdata, writeData)
+  Mem.io.wmask    := Mux(wfire, io.wstarb, writeMask)
+  Mem.io.wen      := isWrite || (isWaitWaddr && awfire) || (isWaitWdata && wfire)
+
+  /* Read address channel */
+  val arready     = RegInit(true.B)
+
+  arready         := isIdle
+  val readAddr    = RegEnable(io.araddr, arfire)
+  val alignedReadAddr = readAddr(awith-1, log2Ceil(size)) ## Fill(log2Ceil(size), "b0".U)
+  val readAddrAligned = readAddr === alignedReadAddr
+  assert(readAddrAligned)
+  Mem.io.ren      := arfire
+  Mem.io.raddr    := io.araddr
+  val readData    = RegEnable(Mem.io.rdata, arfire)
+
+  /* Read data channel */
+  val rvalid      = RegEnable(Bool(), false.B, isRead)
+  val rdata       = RegEnable(UInt(dwith.W), 0.U, isRead)
+  val rresp       = RegEnable(UInt(2.W), "b00".U(2.W), isRead)
+
+  val delay       = 5
+  val delayCount  = RegInit(0.U(log2Ceil(delay).W))
+  val isFetch     = delayCount === (delay - 1).U
+  delayCount      := MuxCase(0.U, Seq(
+    (isRead && !isFetch) -> (delayCount + 1.U),
+    (isRead && isFetch) -> delayCount,
+  ))
+  rvalid          := Mux(rfire, false.B, Mux(rvalid, rvalid, isFetch))
+  rdata           := readData
+  rresp           := TransactionResponse.okey
+
+}
 }
 
